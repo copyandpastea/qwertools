df377bac6f97ffa743d71f93d2351867d79dffcb
hjkl1 <1161524405@qq.com>
2026-02-11 20:12:22 +0800

    Remove unused code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,8 @@
 	import "./layout.css";
 	// import '../app.css';
 	import favicon from "$lib/assets/favicon.svg";
-	import { tools, toolsByCategory, type Tool } from "$lib/data/tools";
-	import { page } from "$app/stores";
 	import Header from "$lib/components/layout/Header.svelte";
 	import Footer from "$lib/components/layout/Footer.svelte";
-
-	const categories = [
-		{ key: "text", label: "文本工具", tools: toolsByCategory.text },
-		{ key: "image", label: "图片工具", tools: toolsByCategory.image },
-		{ key: "other", label: "其他工具", tools: toolsByCategory.other },
-	];
 
 	let { children } = $props();
 </script>
